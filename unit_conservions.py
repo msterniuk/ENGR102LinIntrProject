21f1603176b9a3fbb97404639a92534035e64dde
Matthew Sterniuk <188925460+msterniuk@users.noreply.github.com>
2025-09-02 11:37:38 -0500

    Updated unit_conservions.py

--- unit_conservions.py
+++ unit_conservions.py
@@ -1,1 +1,42 @@
-#test file
+#By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:  Matthew Sterniuk
+#        Cade Hart
+#        Andrew Himebrook  
+#        Ben Moran      
+# Section: 216
+# Assignment: Lab 3.19
+# Date: 2 September 2025
+#
+#
+# YOUR CODE HERE
+
+
+'''
+Write one program that prompts the user to enter one number, stores it in an appropriately
+named variable, performs the necessary calculations, and outputs the results to the screen
+with proper labels and two (2) decimal places for each unit conversion listed below. Write a
+single program that outputs all conversions named unit_conversions.py.
+
+
+a) Pounds (force) to Newtons
+b) Meters to feet
+c) Atmospheres to kilopascals (kPa)
+d) Watts to BTU per hour
+e) Liters per second to US gallons per minute
+f) Degrees Celsius to degrees Fahrenheit
+
+
+
+Example output (using input 1):
+Please enter the quantity to be converted: 1
+1.00 pounds force is equivalent to 4.45 newtons
+1.00 meters is equivalent to 3.28 feet
+1.00 atmospheres is equivalent to 101.33 kilopascals
+1.00 watts is equivalent to 3.41 BTU per hour
+1.00 liters per second is equivalent to 15.85 US gallons per
+minute
+
+'''
